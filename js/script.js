e27719feb8791c310846bf97479e5e99c0950bfd
KellyTTan <60084565+KellyTTan@users.noreply.github.com>
2020-10-07 12:12:28 -1000

    corrected spelling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function validate() {
     if (document.getElementById('tc').checked) {
-        alert("By checking this box, you have read and understand the Terms & Conditions.");
+        alert("By checking this box, you have read and understood the Terms & Conditions.");
     }
 }
